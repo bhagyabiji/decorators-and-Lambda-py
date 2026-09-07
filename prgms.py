d834c082331026a5a1b5fcbfddede4a3036c252f
bhagyabiji <bhagyabanand@gmail.com>
2026-09-07 15:38:44 +0530

    decorators

---
+++ prgms.py
@@ -0,0 +1,60 @@
+#Decorator example
+    #A basic decorator that uppercases the return value of the decorated function.
+
+def changecase(func):
+  def myinner():
+    return func().upper()
+  return myinner
+
+@changecase  #changecase fn is a decorator
+def myfunction():
+  return "Hello Sally"
+
+print(myfunction())
+
+print("------------")
+
+#Using the @changecase decorator on two functions
+def myfun(funct):
+  def inner():
+    return funct().upper()
+  return (inner)
+
+@myfun
+def goodmrng():
+  return "good mrng"
+
+@myfun
+def hello():
+  return "helllo welcome"
+
+print(goodmrng())
+print(hello())
+
+print("------------")
+
+#Functions with arguments can also be decorated
+def argu(func):
+  def inn(x):
+    return func(x).upper()
+  return inn
+
+@argu
+def argmnt(n):
+  return "hello " + n
+
+print(argmnt("john"))
+
+print("------------")
+
+#lambda
+x = lambda a : a+6
+print(x(2))
+
+m = lambda a,b : a*b 
+print(m(2,3))
+
+p = lambda r,s,t : r+s+t
+print(p(4,6,2))
+
+
